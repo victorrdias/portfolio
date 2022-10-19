ecb549a357b1de9887c977abcc5a70eb2d8709b9
victorrdias <vi_dias_15@hotmail.com>
2022-10-19 19:09:09 -0300

    fix vercel deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
 import MyProjects from "../components/projects/MyProjects";
-import Projects from "../components/projects/Projects";
 import ScrollToTop from "../components/scrollToTop";
 import Skills from "../components/skills";
 import Subhero from "../components/Subhero";
